1a9e171a84ed9a8c84a09e4425d8ecaa83f80e09
James Bradlee <james@ihacks.dev>
2021-06-24 06:44:57 +0200

    ci #patch

--- scripts/getLatestVersion.ts
+++ scripts/getLatestVersion.ts
@@ -6,4 +6,4 @@ import {
 } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import output from "./.output.ts";
 
-output(Deno.env.get("GITHUB_SECRET") || "none");
+output(Deno.env.get("GITHUB_SECRET") || "");
