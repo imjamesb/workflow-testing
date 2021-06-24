66e947a82b9b1b85124647ebfac290f130706bda
James Bradlee <james@ihacks.dev>
2021-06-24 05:43:31 +0200

    ci #patch

--- scripts/latest.ts
+++ scripts/latest.ts
@@ -1,9 +1,9 @@
 import $ from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
-import { maxSatisfying } from "https://deno.land/x/semver@v1.4.0/mod.ts";
+import { maxSatisfying, valid } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 let max = maxSatisfying(
   [...(await $`git ls-remote -t .`.stdout()).matchAll(
     /refs\/tags\/([^\n]*)/gi,
-  )].map((_) => _[1]),
+  )].map((_) => _[1]).filter((version) => valid(version)),
   ">= 0",
 );
 // We set to 0.0.0, because the ci will ugrade to 0.1.0 automatically.
