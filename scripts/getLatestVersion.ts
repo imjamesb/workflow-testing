0292e43b127bd0ee8b859877cfcf48eeba4788d4
James Bradlee <james@ihacks.dev>
2021-06-24 06:49:58 +0200

    ci #patch

--- scripts/getLatestVersion.ts
+++ scripts/getLatestVersion.ts
@@ -1,12 +1,23 @@
 // Imports
 import _$ from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
 import {
-  maxSatisfying as _maxSatisfying,
-  valid as _valid,
+  maxSatisfying as maxSatisfying,
+  valid as valid,
 } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import output from "./.output.ts";
 
 const secret = Deno.env.get("GITHUB_TOKEN");
 if (!secret) throw new Error("Missing github token!");
 
-output("Going great!");
+const latest = maxSatisfying(
+  [...(await $`git ls-remote -t .`.stdout()).matchAll(
+    /refs\/tags\/([^\n]*)/gi,
+  )].map((_) => _[1]).filter((version) => valid(version)),
+  ">= 0",
+);
+if (!latest) {
+  output("0.0.0");
+  Deno.exit(0);
+}
+
+output(latest);
