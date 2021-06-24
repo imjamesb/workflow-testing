a262ef62a0050af7e1499122dc7e77b2e2d61ecf
James Bradlee <james@ihacks.dev>
2021-06-24 05:16:00 +0200

    ci #patch

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,7 +1,4 @@
-import $ from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
 import { inc } from "https://deno.land/x/semver@v1.4.0/mod.ts";
-console.log(Deno.env.toObject());
-console.log(Deno.args);
 
 let [version, message] = Deno.args;
 if (version === "0.0.0") {
@@ -18,3 +15,5 @@ if (version === "0.0.0") {
   console.error("error: no new tag");
   Deno.exit(1);
 }
+
+Deno.stdout.write(new TextEncoder().encode(version));
