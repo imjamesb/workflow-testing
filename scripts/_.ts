b051b78a3211bbb4df37faa695aaa1c192c7a72d
James Bradlee <james@ihacks.dev>
2021-06-24 07:07:16 +0200

    ci #patch

--- scripts/_.ts
+++ scripts/_.ts
@@ -14,9 +14,11 @@ const secret = Deno.env.get("GITHUB_TOKEN");
 if (!secret) throw new Error("Missing github token!");
 
 const latest = maxSatisfying(
-  [...(await $`git ls-remote -t .`.stdout()).matchAll(
-    /refs\/tags\/([^\n]*)/gi,
-  )].map((_) => _[1]).filter((version) => valid(version)),
+  [...(await $
+    `git ls-remote -t https://x-access-token:\${GITHUB_TOKEN}@github.com/\${GITHUB_REPOSITORY}`
+    .stdout()).matchAll(
+      /refs\/tags\/([^\n]*)/gi,
+    )].map((_) => _[1]).filter((version) => valid(version)),
   ">= 0",
 );
 if (!latest) {
