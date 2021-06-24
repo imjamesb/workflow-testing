2930d061eda3e54b3abb01bd9dc03fc2ab58c2ed
James Bradlee <james@ihacks.dev>
2021-06-24 07:05:26 +0200

    ci #patch

--- scripts/_.ts
+++ scripts/_.ts
@@ -1,10 +1,13 @@
 // Imports
 import $ from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
 import {
-  maxSatisfying as maxSatisfying,
-  valid as valid,
+  inc,
+  maxSatisfying,
+  valid,
 } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import output from "./.output.ts";
+
+let version!: string | null;
 
 const secret = Deno.env.get("GITHUB_TOKEN");
 if (!secret) throw new Error("Missing github token!");
@@ -16,6 +19,27 @@ const latest = maxSatisfying(
   ">= 0",
 );
 if (!latest) {
-  output("0.10");
-  Deno.exit(0);
+  version = "0.1.0";
 }
+if (latest && version !== undefined) {
+  const msg = Deno.args[0];
+  if (!msg) Deno.exit(0);
+  const result = msg.match(/\#(pre|patch|minor|major)/i);
+  if (!result) Deno.exit(0);
+  const newV = inc(latest!, result[1] as "pre");
+  if (!newV) throw new Error("Could increment version!");
+  version = newV;
+}
+
+if (!version) Deno.exit(0);
+
+await $`
+  rm -rf version.ts && echo "export default \"\${NEWTAG}\";" >> version.ts
+  git config user.name "\${GITHUB_ACTOR}"
+  git config user.email "\${GITHUB_ACTOR}@users.noreply.github.com"
+  git add .
+  git commit -m "Incremented version to \${NEWTAG}"
+  git tag \${NEWTAG}
+  git remote set-url origin https://x-access-token:\${GITHUB_TOKEN}@github.com/\${GITHUB_REPOSITORY}
+  git push -u origin \${NEWTAG}
+`;
