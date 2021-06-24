7e0d7d578101bb4e601b57734b9235e83834d992
James Bradlee <james@ihacks.dev>
2021-06-24 04:57:59 +0200

    ci

---
+++ scripts/latest.ts
@@ -0,0 +1,11 @@
+import $ from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
+import { maxSatisfying } from "https://deno.land/x/semver@v1.4.0/mod.ts";
+let max = maxSatisfying(
+  [...(await $`git ls-remote -t .`.stdout()).matchAll(
+    /refs\/tags\/([^\n]*)/gi,
+  )].map((_) => _[1]),
+  ">= 0",
+);
+// We set to 0.0.0, because the ci will ugrade to 0.1.0 automatically.
+if (!max) max = "0.0.0";
+await Deno.stdout.write(new TextEncoder().encode(max));
