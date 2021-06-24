68e59d68b40800158a774ac989849db239988a43
James Bradlee <james@ihacks.dev>
2021-06-24 07:19:50 +0200

    ci #patch

--- scripts/_.ts
+++ scripts/_.ts
@@ -54,11 +54,11 @@ if (!version) {
 Deno.writeTextFileSync("version.ts", `export default "${version}";\n`);
 
 await $`
-  git config user.name "${$.env.GITHUB_ACTOR}"
-  git config user.email "${$.env.GITHUB_ACTOR}@users.noreply.github.com"
-  git add .
-  git commit -m "Incremented version to ${version}"
-  git tag ${version}
-  git remote set-url origin https://x-access-token:${$.env.GITHUB_TOKEN}@github.com/${$.env.GITHUB_REPOSITORY}
-  git push -u origin ${version}
+  git config user.name "${$.env.GITHUB_ACTOR}";
+  git config user.email "${$.env.GITHUB_ACTOR}@users.noreply.github.com";
+  git add .;
+  git commit -m "Incremented version to ${version}";
+  git tag ${version};
+  git remote set-url origin https://x-access-token:${$.env.GITHUB_TOKEN}@github.com/${$.env.GITHUB_REPOSITORY};
+  git push -u origin ${version};
 `;
